29f489fc504afd6a14d110eecd0fa14c70dd2fae
João Victor <joao.lenny100@gmail.com>
2022-07-03 12:14:31 -0300

    Sequelize
    - Montando base para utilizar o Sequelize.

---
+++ backend/src/database/migrations/20220703151357-create-progress.js
@@ -0,0 +1,30 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Progresses', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      taskId: {
+        type: Sequelize.NUMBER
+      },
+      inProgress: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Progresses');
+  }
+};
