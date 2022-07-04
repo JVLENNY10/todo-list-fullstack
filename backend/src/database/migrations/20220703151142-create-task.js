17d1311ead14a0bb449009f2bb4c7d1a5fa47b53
João Victor <joao.lenny100@gmail.com>
2022-07-03 22:45:37 -0300

    Sequelize: Migrations
    - Ajustando migration de final create-task.

--- backend/src/database/migrations/20220703151142-create-task.js
+++ backend/src/database/migrations/20220703151142-create-task.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Tasks', {
+    await queryInterface.createTable('tasks', {
       id: {
         allowNull: false,
         autoIncrement: true,
