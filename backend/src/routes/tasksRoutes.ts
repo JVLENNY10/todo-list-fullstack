8eca09c0e30a5bbf2fc966f83b7a97f71b5d93f4
João Victor <joao.lenny100@gmail.com>
2022-07-04 17:39:21 -0300

    Tasks Routes
    - Criando rota do tipo put para update.

--- backend/src/routes/tasksRoutes.ts
+++ backend/src/routes/tasksRoutes.ts
@@ -4,10 +4,11 @@ import TasksControllers from '../controllers/TasksControllers';
 const routes = Router();
 const tasksControllers = new TasksControllers();
 
-const { create, destroy, findAll } = tasksControllers;
+const { create, destroy, findAll, update } = tasksControllers;
 
 routes.get('/', findAll);
 routes.post('/', create);
 routes.delete('/:id', destroy);
+routes.put('/', update);
 
 export default routes;
