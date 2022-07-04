6bafa76ca292dbe8d0799c6680171a740ba6b241
João Victor <joao.lenny100@gmail.com>
2022-07-04 16:14:15 -0300

    Tasks Controllers
    - Criando método create.

--- backend/src/controllers/TasksControllers.ts
+++ backend/src/controllers/TasksControllers.ts
@@ -8,6 +8,12 @@ class TasksControllers {
     this.tasksServices = new TasksServices();
   }
 
+  public create = async (req: Request, res: Response) => {
+    const { newTask } = req.body;
+    const task = await this.tasksServices.create(newTask);
+    return res.status(201).json(task);
+  }
+
   public findAll = async (_req: Request, res: Response) => {
     const tasks = await this.tasksServices.findAll();
     return res.status(200).json(tasks);
