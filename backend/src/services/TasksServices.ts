ace7f182985f875ae0495a5b17fdfaa85f46482a
João Victor <joao.lenny100@gmail.com>
2022-07-04 16:14:05 -0300

    Tasks Services
    - Criando método create.

--- backend/src/services/TasksServices.ts
+++ backend/src/services/TasksServices.ts
@@ -1,6 +1,11 @@
 import Task from '../database/models/Task';
 
 class TasksServices {
+  public create = async (newTask: string) => {
+    const task = await Task.create({ task: newTask, inProgress: false });
+    return task;
+  }
+
   public findAll = async () => {
     const tasks = await Task.findAll({ attributes: { exclude: ['id'] } });
     return tasks;
