653894a7d60068e9cf2c7acfa1c8f122e28d476c
João Victor <joao.lenny100@gmail.com>
2022-07-05 08:31:54 -0300

    Front: TodoList
    - Renderizando tarefas vindas do banco de dados.

--- frontend/src/components/TodoList.jsx
+++ frontend/src/components/TodoList.jsx
@@ -1,13 +1,42 @@
+import Task from './Task';
 import { Component } from 'react';
 
 class TodoList extends Component {
+  constructor() {
+    super();
+    this.state = { tasks: [''] };
+    this.findAll = this.findAll.bind(this);
+  }
+
+  componentDidMount() {
+    this.findAll();
+  }
+
+  async findAll() {
+    try {
+      const response = await fetch('http://localhost:3333');
+      const tasks = await response.json();
+      this.setState({ tasks: tasks });
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   render() {
+    const { tasks } = this.state;
+
     return (
       <section>
         <input id='task-text' />
         <button id='add-task'>Adicionar</button>
 
-        <ul id='todo-list'></ul>
+        <ul id='todo-list'>
+          {
+            tasks.map((task, index) => (
+              <Task taskId={ index } taskValue={ task.task } />
+            ))
+          }
+        </ul>
       </section>
     );
   }
